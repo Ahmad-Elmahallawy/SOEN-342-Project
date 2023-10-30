d9fc14e12a6d1e2012af984a41d32ab229541877
clara-gagnon <clarag2002@gmail.com>
2023-10-29 20:11:12 -0400

    Update Temperature class

--- Iteration1/Implementation/src/Temperature.java
+++ Iteration1/Implementation/src/Temperature.java
@@ -1,6 +1,17 @@
 
 public class Temperature {
+	private double temp;
 	
-	public Temperature() {}
-
+	public Temperature(double t) {
+		temp = t;
+	}
+	
+	public void setTemperature(double t) {
+		temp = t;
+	}
+	
+	public double getTemperature() {
+		return temp;
+	}
+	
 }
