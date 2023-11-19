e7fc0d111c703aba62d1be05d517c7332e74286f
Ahmad Elmahallawy <84809147+Ahmad-Elmahallawy@users.noreply.github.com>
2023-11-19 15:53:05 -0500

    Created ReadTable.java class and added all methods

---
+++ Iteration2/interaction-diagrams/ReadTable.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class ReadTable {
+
+	private HashMap<DeployedSensor, Temperature> reads = new HashMap<DeployedSensor, Temperature> ();
+	
+	// creates Read
+	public void makeNewRead(DeployedSensor sensor, Temperature temperature) {
+		reads.put(sensor, temperature);
+	}
+
+	// removes Read
+	public void removeRead(DeployedSensor sensor) {
+		reads.remove(sensor);
+	}
+	
+	// finds read with mapping of (sensor, temperature) given the sensor, returns the temperature
+	public Temperature findTemperature(DeployedSensor sensor) {
+		return reads.get(sensor);
+	}
+}
