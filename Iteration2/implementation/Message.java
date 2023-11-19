932fdb44b0d044e45347b4435aa4e989cc2fcb0b
Ahmad Elmahallawy <84809147+Ahmad-Elmahallawy@users.noreply.github.com>
2023-11-19 15:58:17 -0500

    Created Message.java class and added all methods

---
+++ Iteration2/implementation/Message.java
@@ -0,0 +1,23 @@
+public class Message {
+
+    public static void sensorAlreadyDeployed() {
+        System.out.println("Sensor already deployed");
+    }
+
+    public static void sensorNotDeployed() {
+        System.out.println("Sensor not deployed");
+    }
+
+    public static void locationAlreadyCovered() {
+        System.out.println("Location already covered");
+    }
+
+    public static void locationNotCovered() {
+        System.out.println("Location not covered");
+    }
+    
+    public static void successMessage() {
+        System.out.println("Ok");
+    }
+
+}
