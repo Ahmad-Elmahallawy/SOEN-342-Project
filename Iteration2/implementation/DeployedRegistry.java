733399223075fda773cef7476742499570fcd927
Vanessa DP <90293133+vanessadp17@users.noreply.github.com>
2023-11-19 16:09:11 -0500

    Created DeployedRegistry.java and added all its methods

---
+++ Iteration2/implementation/DeployedRegistry.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class DeployedRegistry {
+    private ArrayList<DeployedSensor> deployed = new ArrayList<DeployedSensor> ();
+
+    // add sensor to deployed given the sensor
+    public DeployedSensor addDeployed(Sensor sensor) {
+        if (checkUniqueID(sensor.getID())) {
+            DeployedSensor depl = new DeployedSensor(sensor.getID());
+		    deployed.add(depl);
+            return depl;
+        }
+        else {
+            Message.nonUniqueSensorID();
+            return null;
+        }
+	}
+
+    // remove sensor from deployed
+	public void removeDeployed(DeployedSensor sensor) {
+		deployed.remove(sensor);
+	}
+
+    // finds sensor in deployed from sid, returns sensor 
+    public DeployedSensor findDeployed(int sid) {
+        for (DeployedSensor sensor : deployed) {
+            if (sensor.getID() == sid) {
+                return sensor;
+            }
+        }
+        return null;
+    }
+    // internal method
+    // check unique id of sensor
+    private boolean checkUniqueID(int sid) {
+		for (DeployedSensor sensor : deployed) {
+            if (sensor.getID() == sid) {
+                return false;
+            }
+        }
+        return true;
+	}
+}
+    
