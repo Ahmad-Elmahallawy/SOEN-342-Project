b0e18f65635b0a7e50409a87d7b3f5ae058d45e3
Vanessa DP <90293133+vanessadp17@users.noreply.github.com>
2023-11-19 17:10:34 -0500

    Added methods addDeployedSensor, removeDeployedSensor, and findDeployedSensor to SensorRegistry.java

--- Iteration2/implementation/SensorRegistry.java
+++ Iteration2/implementation/SensorRegistry.java
@@ -2,19 +2,37 @@ import java.util.ArrayList;
 
 public class SensorRegistry {
 	private ArrayList<Sensor> sensors = new ArrayList<Sensor> ();
-    private DeployedRegistry deployedRegistry;
+    	private DeployedRegistry deployedRegistry;
 
 	public SensorRegistry(DeployedRegistry dreg) {
 		deployedRegistry = dreg;
 	}
 
 	// finds sensor in sensorRegistry from sid, returns sensor 
-    public Sensor findSensor(int sid) {
-        for (Sensor sensor : sensors) {
-            if (sensor.getID() == sid) {
-                return sensor;
-            }
-        }
-        return null;
-    }
+    	public Sensor findSensor(int sid) {
+        	for (Sensor sensor : sensors) {
+            		if (sensor.getID() == sid) {
+                		return sensor;
+            		}
+        	}
+        	return null;
+    	}
+	
+	public DeployedSensor addDeployedSensor(Sensor sensor) {
+        	DeployedSensor deployedSensor = deployedRegistry.addDeployed(sensor);
+		// Replace sensor in sensorRegistry with deployedSensor
+        	sensors.set(sensors.indexOf(sensor), deployedSensor);
+		return deployedSensor;
+	}
+
+	public void removeDeployedSensor(DeployedSensor sensor) {
+		deployedRegistry.removeDeployed(sensor);
+		// Replace deployedSensor in sensorRegistry with sensor
+		Sensor undeployedSensor = new Sensor(sensor.getID());
+        	sensors.set(sensors.indexOf(sensor), undeployedSensor);
+	}
+	
+    	public DeployedSensor findDeployedSensor(int sid) {
+		return deployedRegistry.findDeployed(sid);
+	}
 }
