0a8a45adc4a0913db5b7bfd2e17e863c1b9cd651
Ahmad Elmahallawy <84809147+Ahmad-Elmahallawy@users.noreply.github.com>
2023-11-19 17:16:57 -0500

    Added deploySensor function to Console.java

--- Iteration2/implementation/Console.java
+++ Iteration2/implementation/Console.java
@@ -8,17 +8,27 @@ public class Console {
 	private MapTable mapTable;
 	private ReadTable readTable;
 
+	// constructor
 	public Console(SensorRegistry sensorRegistry, LocationRegistry locationRegistry, MapTable mapTable, ReadTable readTable) {
 		this.sensorRegistry = sensorRegistry;
 		this.locationRegistry = locationRegistry;
 		this.mapTable = mapTable;
 		this.readTable = readTable;
 	}
-
+	
 	public Temperature readTemperature(Location location) {
 		DeployedSensor sensor = mapTable.findSensor(location);
 		Temperature temperature = readTable.findTemperature(sensor);
 		return temperature;
+	}
+
+	public void deploySensor(Sensor sensor, Location location, Temperature temperature) {
+		// Add sensor to deployRegistry
+		DeployedSensor deployed = sensorRegistry.addDeployedSensor(sensor);
+		// Create new map
+		mapTable.makeNewMap(deployed, location);
+		// Create new read
+		readTable.makeNewRead(deployed, temperature);
 	}
 
 } 
