e84f66ce63b4a724aaa2287dbe913ad6bb05878a
Ahmad Elmahallawy <84809147+Ahmad-Elmahallawy@users.noreply.github.com>
2023-11-19 16:20:50 -0500

    Added Console.java class

---
+++ Iteration2/implementation/Console.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Console {
+	
+	private SensorRegistry sensorRegistry;
+	private LocationRegistry locationRegistry;
+	private MapTable mapTable;
+	private ReadTable readTable;
+
+	public Console(SensorRegistry sensorRegistry, LocationRegistry locationRegistry, MapTable mapTable, ReadTable readTable) {
+		this.sensorRegistry = sensorRegistry;
+		this.locationRegistry = locationRegistry;
+		this.mapTable = mapTable;
+		this.readTable = readTable;
+	}
+
+} 
