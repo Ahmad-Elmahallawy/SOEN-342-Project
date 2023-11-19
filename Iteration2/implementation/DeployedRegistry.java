1d5dda55de091626e9c1480cc05f593ad49d5013
Vanessa DP <90293133+vanessadp17@users.noreply.github.com>
2023-11-19 18:16:13 -0500

    Added getAllDeployed method to DeployedRegistry.java

--- Iteration2/implementation/DeployedRegistry.java
+++ Iteration2/implementation/DeployedRegistry.java
@@ -1,44 +1,51 @@
 import java.util.ArrayList;
 
 public class DeployedRegistry {
-    private ArrayList<DeployedSensor> deployed = new ArrayList<DeployedSensor> ();
+    	private ArrayList<DeployedSensor> deployed = new ArrayList<DeployedSensor> ();
 
-    // add sensor to deployed given the sensor
-    public DeployedSensor addDeployed(Sensor sensor) {
-        if (checkUniqueID(sensor.getID())) {
-            DeployedSensor depl = new DeployedSensor(sensor.getID());
-		    deployed.add(depl);
-            return depl;
-        }
-        else {
-            Message.nonUniqueSensorID();
-            return null;
-        }
+    	// add sensor to deployed given the sensor
+    	public DeployedSensor addDeployed(Sensor sensor) {
+        	if (checkUniqueID(sensor.getID())) {
+            		DeployedSensor depl = new DeployedSensor(sensor.getID());
+		    	deployed.add(depl);
+            		return depl;
+        	}
+        	else {
+            		Message.nonUniqueSensorID();
+            		return null;
+        	}
 	}
 
-    // remove sensor from deployed
+    	// remove sensor from deployed
 	public void removeDeployed(DeployedSensor sensor) {
 		deployed.remove(sensor);
 	}
 
-    // finds sensor in deployed from sid, returns sensor 
-    public DeployedSensor findDeployed(int sid) {
-        for (DeployedSensor sensor : deployed) {
-            if (sensor.getID() == sid) {
-                return sensor;
-            }
-        }
-        return null;
-    }
-    // internal method
-    // check unique id of sensor
-    private boolean checkUniqueID(int sid) {
+    	// finds sensor in deployed from sid, returns sensor 
+    	public DeployedSensor findDeployed(int sid) {
+        	for (DeployedSensor sensor : deployed) {
+            		if (sensor.getID() == sid) {
+                	return sensor;
+            		}
+        	}
+        	return null;
+    	}
+	
+    	// internal method
+    	// check unique id of sensor
+    	private boolean checkUniqueID(int sid) {
 		for (DeployedSensor sensor : deployed) {
-            if (sensor.getID() == sid) {
-                return false;
-            }
-        }
-        return true;
+            		if (sensor.getID() == sid) {
+                		return false;
+            		}
+        	}
+        	return true;
 	}
+
+	// ----- get objects to write to the text file -----
+    	public ArrayList<DeployedSensor> getAllDeployed() {
+		return deployed;
+	}
+	// ------------------------------------------------
 }
     
